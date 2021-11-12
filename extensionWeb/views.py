e11d14176201a802acb1e7438826cdafc05d6f5e
Anubhav Gupta <mail.anubhav06@gmail.com>
2021-11-12 21:53:51 +0530

    project, app created

---
+++ extensionWeb/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponseNotAllowed
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('It works!')
